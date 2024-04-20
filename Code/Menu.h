f5ee03ec497d7444d14b42a3c011c1116c7a3976
Bhavna Chummun <BC570@live.mdx.ac.uk>
2024-04-20 13:32:05 +0000

    Updated Menu.h

--- Code/Menu.h
+++ Code/Menu.h
@@ -1,6 +1,7 @@
 #ifndef MENU_H
 #define MENU_H
 
+// Use of standard and OS independant C++ libraries. 
 #include <iostream>
 #include <vector>
 #include <list>
@@ -14,22 +15,24 @@
 
 class Menu {
 private:
+    // Structure to store the details of each menu item.
     struct MenuItem {
         std::string name;
         std::string ingredients;
         std::string price;
     };
 
-    std::vector<std::list<MenuItem>> table;
-    int tableSize;
-
+    // Private methods.
     int hashFunction(const std::string& key) const;
     void insertMenuItem(const std::string& name, const std::string& ingredients,
                         const std::string& price);
     void loadItemsFromCSV(const std::string& filename);
 
 public:
+    // Public methods.
     Menu(int size = 101);
+    std::vector<std::list<MenuItem>> table;
+    int tableSize;
     void displayMenuItems() const;
     void searchMenuItem();
     std::string getPrice(const std::string& keyword);
