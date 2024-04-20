d5fd13240c808d6d5413a580cd12a706890a5764
Bhavna Chummun <BC570@live.mdx.ac.uk>
2024-04-20 13:31:29 +0000

    Updated Menu.cpp

--- Code/Menu.cpp
+++ Code/Menu.cpp
@@ -1,10 +1,12 @@
 #include "menu.h"
 
+// Implement hash function to calculate index.
 int Menu::hashFunction(const std::string& key) const {
     std::hash<std::string> hashFunc;
     return hashFunc(key) % tableSize;
 }
 
+// Function to insert menu items to the hash table.
 void Menu::insertMenuItem(const std::string& name, 
                           const std::string& ingredients, 
                           const std::string& price) {
@@ -13,20 +15,19 @@ void Menu::insertMenuItem(const std::string& name,
         if (item.name == name) {
             item.ingredients = ingredients;
             item.price = price;
-            return;
+            return; 
         }
     }
     table[index].push_back({name, ingredients, price});
 }
 
+// Function to load menu items from a CSV file to the hash table.
 void Menu::loadItemsFromCSV(const std::string& filename) {
     std::ifstream file(filename);
     if (!file.is_open()) {
         std::cerr << "Error opening file: " << filename << std::endl;
         showMenu();
-    }
-    else
-    {
+    } else {
         std::cout << "File input successful" << std::endl;
     }
 
@@ -34,20 +35,26 @@ void Menu::loadItemsFromCSV(const std::string& filename) {
     while (getline(file, line)) {
         std::stringstream ss(line);
         if (getline(ss, name, ',') && getline(ss, ingredients, ',') 
-            && getline(ss, price)) {
+            && getline(ss, price)) 
+        {
             insertMenuItem(name, ingredients, price);
-        } else {
+        } 
+        else 
+        {
             std::cerr << "Failed to parse line: " << line << std::endl;
         }
     }
     file.close();
 }
 
+// Constructor to initialize Menu object. 
 Menu::Menu(int size) : table(size), tableSize(size) {}
 
+// Function to display menu items stored in the hash table.
 void Menu::displayMenuItems() const {
     for (int i = 0; i < tableSize; ++i) {
-            if (!table[i].empty()) {
+            if (!table[i].empty()) 
+            {
                 for (const auto& item : table[i]) {
                     std::cout << "\nItem Key: " << i << "\nItem Name: " 
                               << item.name
@@ -58,59 +65,61 @@ void Menu::displayMenuItems() const {
         }
 }
 
+// Function to search for menu items by their name or an ingredient.
 void Menu::searchMenuItem() {
     std::string filename;
 
-        std::string keyword;
-        std::cout << "Enter the item name or ingredients you want"
-                  << "to search for: ";
-        std::cin.ignore();  // Ignore the newline character left in the stream
-        std::getline(std::cin, keyword);
+    std::string keyword;
+    std::cout << "Enter the item name or ingredients you want "
+                << "to search for: ";
+    std::cin.ignore();  // Ignore the newline character left in the stream
+    std::getline(std::cin, keyword);
 
-        // Turn user input to lowercase for more effective comparison.
-        keyword = toLowercase(keyword);
-        bool found = false;
-        for (int i = 0; i < tableSize; ++i) {
-            for (const auto& item : table[i]) {
-                /* Turn menu item to lowercase for more effective comparison 
-                    against user input.*/
-                if (toLowercase(item.name).find(keyword) != std::string::npos || 
-                    toLowercase(item.ingredients).find(keyword) != 
-                                std::string::npos) 
-                    {
-                    std::cout << "\nItem Found\nItem Key: " << i 
-                                << "\nItem Name: " 
-                                << item.name
-                                << "\nIngredients: " << item.ingredients 
-                                << "\nPrice: " << item.price << std::endl;
-                    found = true;
-                }
+    // Turn user input to lowercase for more effective comparison.
+    keyword = toLowercase(keyword);
+    bool found = false;
+    for (int i = 0; i < tableSize; ++i) {
+        for (const auto& item : table[i]) {
+            // Turn menu item to lowercase for more effective comparison 
+            // against user input.
+            if (toLowercase(item.name).find(keyword) != std::string::npos || 
+                toLowercase(item.ingredients).find(keyword) != 
+                            std::string::npos) 
+            {
+                std::cout << "\nItem Found\nItem Key: " << i 
+                        << "\nItem Name: " << item.name
+                        << "\nIngredients: " << item.ingredients 
+                        << "\nPrice: " << item.price << std::endl;
+                found = true;
             }
         }
+    }
 
-        if (!found) {
-            std::cout << "Item not found in the menu." << std::endl;
-        }
+    if (!found) 
+    {
+        std::cout << "Item not found in the menu." << std::endl;
+    }
 }
 
+// Function to get the price of a menu item.
 std::string Menu::getPrice(const std::string& keyword) {
     // Turn user input to lowercase for more effective comparison.
     std::string lowercaseKeyword = toLowercase(keyword);
     for (int i = 0; i < tableSize; ++i) {
         for (const auto& item : table[i]) {
-            /* Turn menu item to lowercase for more effective comparison 
-                against user input.*/
+            // Turn menu item to lowercase for more effective comparison 
+            // against user input.
             if (toLowercase(item.name).find(lowercaseKeyword) != std::string::npos) 
-                {
-                return item.price; // Return the price if found
+            {
+                return item.price; // Return the price if found.
             }
         }
     }
     std::cout << "Item not found in the menu." << std::endl;
-    return 0; // Return a default value if not found
+    return 0; 
 }
 
-
+// Function to convert a string to lowercase.
 std::string Menu::toLowercase(const std::string& upperString) {
             std::string lowerString = upperString;
         std::transform(lowerString.begin(), lowerString.end(), 
@@ -120,6 +129,7 @@ std::string Menu::toLowercase(const std::string& upperString) {
         return lowerString;
 }
 
+// Function to display the menu based on user input of a filename.
 void Menu::showMenu() {
         std::string filename;
 
@@ -129,45 +139,49 @@ void Menu::showMenu() {
         loadItemsFromCSV(filename);
 }
 
+// Function to add new items to a menu.
 void Menu::addMenuItems() {
     std::string filename;
-        std::cout << "Enter the file name you want to edit: ";
-        std::cin >> filename;
 
-        std::string name, ingredients, price;
+    std::cout << "Enter the file name you want to edit: ";
+    std::cin >> filename;
 
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::string name, ingredients, price;
 
-        std::cout << "Enter Menu Item: ";
-        std::getline(std::cin, name);
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-        std::cout << "Enter Ingredients: ";
-        std::getline(std::cin, ingredients);
+    std::cout << "Enter Menu Item: ";
+    std::getline(std::cin, name);
 
-        std::cout << "Enter Price: ";
-        std::getline(std::cin, price);
+    std::cout << "Enter Ingredients: ";
+    std::getline(std::cin, ingredients);
 
-        insertMenuItem(name, ingredients, price);
+    std::cout << "Enter Price: ";
+    std::getline(std::cin, price);
+
+    insertMenuItem(name, ingredients, price);
         
-        std::ofstream file(filename, std::ios::app);
-        for (int i = 0; i < tableSize; ++i) {
-            for (const auto& item : table[i]) {
-                file << item.name << "," << item.ingredients << "," 
-                     << item.price << "\n";
-            }
+    std::ofstream file(filename, std::ios::app);
+    for (int i = 0; i < tableSize; ++i) {
+        for (const auto& item : table[i]) {
+            file << item.name << "," << item.ingredients << "," 
+                << item.price << "\n";
         }
-        file.close();
+    }
+    
+    file.close();
 }
 
+// Function to remove items from a menu.
 void Menu::removeMenuItems() {
     std::string filename;
 
-    // Load current menu items
+    // Load current menu items.
     std::cout << "Enter the file name you want to edit: ";
     std::cin >> filename;
     loadItemsFromCSV(filename);
 
-    // Display current items
+    // Display current items.
     displayMenuItems();
 
     int key;
@@ -191,8 +205,8 @@ void Menu::removeMenuItems() {
         table[key].clear();
         std::cout << "Item removed successfully." << std::endl;
     } else {
-        /* If more than one item has the same key, ask for item name for 
-           efficient removal.*/
+        // If more than one item has the same key, ask for item name for 
+        // efficient removal.
         std::string nameToRemove;
         std::cout << "Multiple items found at this key. "
                   << "Enter the name of the item to remove: ";
@@ -229,27 +243,31 @@ void Menu::removeMenuItems() {
     file.close();
 }
 
+// Function to edit a menu by adding or removing items.
 void Menu::editCSVFile() {
-    int choice;
-        std::cout << "Edit Menu:\n";
-        std::cout << "1. Add Menu Items\n";
-        std::cout << "2. Remove Menu Items\n";
-        std::cout << "Enter your choice (1 or 2): ";
-        std::cin >> choice;
-        
-        switch (choice) {
-            case 1:
-                addMenuItems();
-                break;
-            case 2:
-                removeMenuItems();
-                break;
-            default:
-                std::cout << "Invalid choice. Exiting program." << std::endl;
-                break;
-        }
+    int choice;    
+
+    // Clear the hash before editing.
+    for (int i = 0; i < tableSize; ++i) {
+        table[i].clear();
+    }
+
+    std::cout << "Edit Menu:\n";
+    std::cout << "1. Add Menu Items\n";
+    std::cout << "2. Remove Menu Items\n";
+    std::cout << "Enter your choice (1 or 2): ";
+    std::cin >> choice;
+
+    if (choice == 1) {
+        addMenuItems();
+    } else if (choice == 2) {
+        removeMenuItems();
+    } else {
+        std::cout << "Invalid choice. Exiting program." << std::endl;
+    }
 }
 
+// Function to load menu items from a CSV file.
 void Menu::getloadMenuFromCSV(const std::string& filename) {
     loadItemsFromCSV(filename); 
 }
